41e97741c4877a81ee38058c2698a3fcb3ada89c
Matheus de Campos <matheus.campos@edu.unifil.br>
2020-10-10 19:03:19 -0300

    feat: criado a entidade cliente e interface.

---
+++ orcamento-crud/Interface/IClient.cs
@@ -0,0 +1,15 @@
+﻿using orcamento_crud.Models;
+using System.Collections.Generic;
+
+
+namespace orcamento_crud.Interface
+{
+    public interface IClient
+    {
+        public IEnumerable<Client> GetlAllClients();
+        public void AddClient(Client client);
+        public void DeleteClient(int? id);
+        public void UpdateClient(Client client);
+        public Client GetClient(int? id);
+    }
+}
